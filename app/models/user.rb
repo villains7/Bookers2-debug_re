1992430a461eccf7d4b0ddb211654e1aa3492ad8
EC2 Default User <ec2-user@ip-172-31-45-240.ec2.internal>
2022-01-31 14:16:59 +0900

    いいね、コメント、検索復習

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books ,dependent: :destroy
+  has_many :book_comments ,dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_one_attached :profile_image
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: {maximum: 50 }
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+end
